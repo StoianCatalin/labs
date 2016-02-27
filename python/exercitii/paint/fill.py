e3880fd768e1e58df8438de919658b9c5e155424
StoianCatalin <shaggy.cata@gmail.com>
2016-02-27 17:11:12 +0200

    Fix somethings

--- python/exercitii/paint/fill.py
+++ python/exercitii/paint/fill.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python
 # *-* coding: UTF-8 *-*
 """Tuxy dorește să împlementeze un nou paint pentru consolă.
-
 În timpul dezvoltării proiectului s-a izbit de o problemă
 pe care nu o poate rezolva singur și a apelat la ajutorul tău.
-
 El dorește să adauge o unealtă care să permită umplerea unei
 forme închise.
-
 Exemplu:
-
 Pornim de la imaginea inițială reprezentată mai jos, trebuie să
 umplem formele în care se află "x":
-
   |-----*------|          |******------|         |************|
   |--x--*------|          |******------|         |************|
   |******------|  ----->  |******------|  -----> |************|
   |-----******-|          |-----******-|         |-----*******|
   |-----*---*--|          |-----*---*--|         |-----*---***|
   |-----*---*-x|          |-----*---*--|         |-----*---***|
-
 """
 # pylint: disable=unused-argument
 
@@ -27,7 +21,6 @@ umplem formele în care se află "x":
 def umple_forma(imagine, punct):
     """Funcția primește reprezentarea imaginii și coordonatele unui
     punct.
-
     În cazul în care punctul se află într-o formă închisă trebuie să
     umple forma respectivă cu caracterul "*"
     """
